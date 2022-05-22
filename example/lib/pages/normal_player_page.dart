0f15978c70876b733e313d343680bc5d09f437e1
tnamikawa <takeshi.namikawa@gmail.com>
2022-05-22 21:35:26 +0900

    update dependency

--- example/lib/pages/normal_player_page.dart
+++ example/lib/pages/normal_player_page.dart
@@ -25,10 +25,8 @@ class _NormalPlayerPageState extends State<NormalPlayerPage> {
               DeviceOrientation.portraitUp
             ],
             controlsConfiguration: BetterPlayerControlsConfiguration(
-              skipForwardText: "パーソナルメニューのチャプタ早送り・巻き戻し部分にチャプタ名の先頭数文字を表示する",
-              skipBackwardText: ""
-              // progressCountDown: true,
-              // loadingColor: Color(0xff48bcc1),
+              progressCountDown: true,
+              loadingColor: Color(0xff48bcc1),
             ));
     _betterPlayerDataSource = BetterPlayerDataSource(
       BetterPlayerDataSourceType.network,
